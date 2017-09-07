445c8d5013838e0289158ca41fa9277bc2389fd8
fslic <fslic@DESKTOP-MCATCHJ>
2017-09-07 22:08:40 +0800

    new demos

--- src/test/java/com/in28minutes/junit/helper/StringHelperParameterizedTest.java
+++ src/test/java/com/in28minutes/junit/helper/StringHelperParameterizedTest.java
@@ -1,11 +1,10 @@
 package com.in28minutes.junit.helper;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.Collection;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
